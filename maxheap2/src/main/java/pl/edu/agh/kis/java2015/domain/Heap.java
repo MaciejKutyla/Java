63d9a8d72295b69e6a93dbe3b5a04844b6524e7b
MaciejKutyla <35468919+MaciejKutyla@users.noreply.github.com>
2018-11-24 12:38:52 +0100

    Kopiec

--- maxheap2/src/main/java/pl/edu/agh/kis/java2015/domain/Heap.java
+++ maxheap2/src/main/java/pl/edu/agh/kis/java2015/domain/Heap.java
@@ -47,4 +47,82 @@ public class Heap {
 		return tab.get(0);
 	}
 
+	public double extractMax(){
+		double temp=tab.get(0);
+		int index=0;
+		for(int i=0;i<heapSize;i++) {
+			if (tab.get(i) > temp) {
+				temp = tab.get(i);
+				index = i;
+			}
+		}
+
+		tab.remove(index);
+		heapSize--;
+
+		int currentIndex=heapSize-1;
+		int parentIndex=parentIndex(currentIndex);
+
+		while( isChildGreaterThanParent(currentIndex, parentIndex) ) {
+			swapElements(currentIndex, parentIndex);
+			currentIndex = parentIndex;
+			parentIndex = parentIndex(currentIndex);
+		}
+		return(temp);
+
+
+	}
+
+	public void deleteMax(){
+		extractMax();
+	}
+
+	public void replace(double value){
+		double temp=tab.get(0);
+		int index=0;
+		for(int i=0;i<heapSize;i++) {
+			if (tab.get(i) > temp) {
+				temp = tab.get(i);
+				index = i;
+			}
+		}
+
+		tab.set(index,value);
+
+		int currentIndex=heapSize-1;
+		int parentIndex=parentIndex(currentIndex);
+
+		while( isChildGreaterThanParent(currentIndex, parentIndex) ) {
+			swapElements(currentIndex, parentIndex);
+			currentIndex = parentIndex;
+			parentIndex = parentIndex(currentIndex);
+		}
+	}
+
+	public Heap heapify(double[] t){
+		Heap heap=new Heap();
+		for (int i=0;i<t.length;i++){
+			heap.insert(t[i]);
+		}
+		return heap;
+	}
+
+	public Heap merge(Heap h){
+		Heap heap=new Heap();
+		for (int i=0;i<heapSize;i++){
+			heap.insert(tab.get(i));
+		}
+		for (int i=0;i<h.heapSize;i++){
+			heap.insert(h.tab.get(i));
+		}
+
+		return heap;
+	}
+
+	public void meld(Heap h){
+		for (int i=0;i<h.heapSize;i++){
+			insert(h.tab.get(i));
+		}
+	}
+
 }
